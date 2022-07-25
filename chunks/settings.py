2ee461f73f7bcf2f2992d9da3ea479c037ae9150
Sidney Uwaya <uwayasidney@gmail.com>
2022-07-25 16:46:29 +0100

    no msg

--- chunks/settings.py
+++ chunks/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-)+5jc%wb)^j#*n)ob^ak+mz$-$u(+!gm7d9-ajxr$3-&ww)1px
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['csvspitting.herokuapp.com']
 
 
 # Application definition
