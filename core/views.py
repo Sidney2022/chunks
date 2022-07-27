d2695138e6e78af56e97161c31753cd374b8e97e
Sidney Uwaya <uwayasidney@gmail.com>
2022-07-27 11:43:50 +0100

    update

--- core/views.py
+++ core/views.py
@@ -76,7 +76,6 @@ def index(request):
         chunk_size = request.POST['chunk_no']
         file = request.FILES.get('doc')
         user = request.user
-        batch_no = 1
         
         if  chunk_size == '' or file == None:
             messages.info(request, 'fields cannot be blank!')
@@ -107,29 +106,6 @@ def index(request):
             os.remove(f'files/{ofile}')
               
         messages.info(request, 'file has been received successfully')
-        
-        
-        try:
-            user_folder = str(user)
-            os.mkdir(f'chunked_files/{user_folder}')
-        except FileExistsError:
-            pass
-        
-        
-        for chunk in pd.read_csv(file, chunksize=int(chunk_size)):
-            new_file = "csv_split" + str(batch_no) + ".csv"
-            chunk.to_csv(f'files/{new_file}', index=False)
-            batch_no += 1
-            
-        with zipfile.ZipFile(f'chunked_files/{user}/{user}-{file}.zip', 'w') as zipF:
-            for file in os.listdir(f'files'):
-                zipF.write(f'files/{file}', compress_type=zipfile.ZIP_DEFLATED)
-
-
-
-        for file in os.listdir('files'):
-            os.remove((f'files/{file}'))        
-        messages.info(request, 'file has been split successfully')
         return redirect('/')
         
     else:
